e5bde51902862b5a30e7852a4940afec0de35d75
Aadi3333 <103951593+Aadi3333@users.noreply.github.com>
2022-09-15 15:18:41 +0530

    Update Code.ino

--- Code.ino
+++ Code.ino
@@ -1,9 +1,9 @@
+#include <Arduino.h>
+#include <string.h>
+
 String Name;
 
-void setup() 
-{  
-  pinMode(LED_BUILTIN, OUTPUT); 
-}
+
 
 void dot() 
 {
@@ -21,6 +21,12 @@ void line()
   delay(1000);
 }
 
+void setup() 
+{  
+  Serial.begin(9600);
+  pinMode(LED_BUILTIN, OUTPUT); 
+}
+
 void loop() 
 {
   // put your main code here, to run repeatedly:
@@ -28,8 +34,9 @@ void loop()
   Name = Serial.readString();
   Name.toUpperCase();
   Serial.println(Name);
-
   blink_name(Name);
+  digitalWrite(LED_BUILTIN,LOW);
+  delay(5000);
  
 }
 
@@ -40,134 +47,160 @@ void blink_name(String Name)
     switch (Name[i]) 
     {
       case 'A':
+      //Morse Code = .-
         dot();
         line();
         break;
       case 'B':
+       //Morse Code = -...
         line();
         dot();
         dot();
         dot();
         break;
       case 'C':
+       //Morse Code = -.-.
         line();
         dot();
         line();
         dot();
         break;
       case 'D':
+       //Morse Code = -..
         line();
         dot();
         dot();
         break;
       case 'E':
+       //Morse Code = .
         dot();
         break;
       case 'F':
+       //Morse Code = ..-.
         dot();
         dot();
         line();
         dot();
         break;
       case 'G':
+       //Morse Code = --.
         line();
         line();
         dot();
         break;
       case 'H':
+       //Morse Code = ....
         dot();
         dot();
         dot();
         dot();
         break;
       case 'I':
+       //Morse Code = ..
         dot();
         dot();
         break;
       case 'J':
+       //Morse Code = .---
         dot();
         line();
         line();
         line();
         break;
       case 'K':
+       //Morse Code = -.-
         line();
         dot();
         line();
         break;
       case 'L':
+       //Morse Code = .-..
         dot();
         line();
         dot();
         dot();
         break;
       case 'M':
+       //Morse Code = --
         line();
         line();
         break;
       case 'N':
-        dot();
+       //Morse Code = -.
         line();
+        dot();
         break;
       case 'O':
+       //Morse Code = ---
         line();
         line();
         line();
         break;
       case 'P':
+      //Morse Code = .--.
         dot();
         line();
         line();
         dot();
         break;
       case 'Q':
+      //Morse Code = --.-
         line();
         line();
         dot();
         line();
         break;
       case 'R':
+      //Morse Code = .-.
         dot();
         line();
         dot();
         break;
       case 'S':
+      //Morse Code = ...
         dot();
         dot();
         dot();
         break;
       case 'T':
+      //Morse Code = -
         line();
         break;
       case 'U':
+      //Morse Code = ..-
         dot();
         dot();
         line();
         break;
       case 'V':
+      //Morse Code = ...-
         dot();
         dot();
         dot();
         line();
         break;
       case 'W':
+      //Morse Code = .--
         dot();
         line();
         line();
         break;
       case 'X':
+      //Morse Code = -..-
         line();
         dot();
         dot();
         line();
         break;
       case 'Y':
+      //Morse Code = -.--
         line();
         dot();
         line();
         line();
         break;
       case 'Z':
+      //Morse Code = --..
         line();
         line();
         dot();
